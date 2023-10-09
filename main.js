e7a3e974bc227fe659792506c67499f41863e393
nikhil <nikhilmahato3337@gmail.com>
2023-10-09 22:48:41 +0530

    error handling

--- main.js
+++ main.js
@@ -16,10 +16,16 @@ form.addEventListener("submit", async (e) => {
     }),
   });
 
-  const { image } = await response.json();
+  if (response.ok) {
+    const { image } = await response.json();
 
-  const result = document.querySelector("#result");
-  result.innerHTML = `<img src="${image}" width="512" />`;
+    const result = document.querySelector("#result");
+    result.innerHTML = `<img src="${image}" width="512" />`;
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+  }
   hideSpinner();
 });
 
